a1c8d35452c2755fe4c39514b011d35c689c313e
Willian Carvalho <wcarvalho@kokku.com.br>
2022-05-17 15:42:13 -0300

    Initial commit

---
+++ MainFile.cpp
@@ -0,0 +1,19 @@
+#include <iostream>;
+
+
+int main() 
+{
+	system("color 1C");
+	//Willian Matheus Souza Carvalho - 17/05/2022
+	/*programa criado para praticar a sintaxe básica do C++*/
+	std::cout << "Vou" << "\n"
+		<< "aprender" << "\n"
+		<< "a" << "\n"
+		<< "programar" << "\n"
+		<< "na" << "\n"
+		<< "Linguagem C++" << std::endl;
+
+
+	system("PAUSE");
+	return 0;
+}
